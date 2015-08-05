ae2a72a8bb32dd58fd42fef0da84ecc536dd78c4
Zoran Rasic <zokiupwork@gmail.com>
2015-08-04 19:31:52 -0700

    Fixed white pixel trim

--- Decode.py
+++ Decode.py
@@ -11,28 +11,53 @@ def trimWhiteSpace(matrix):
     :param matrix: The matrix that contains trailing white pixels.
     :return: The matrix without trailing white pixels.
     """
+
     isUsefulRow = False
-    qrSide = 0
     trimmedMatrix =[]
     for row in matrix:
         if isUsefulRow:
-            #process
+            trimmedMatrix += [row[startPoint: endPoint]]
         elif not rowIsWhiteSpace(row):
             isUsefulRow = True
-            #process the row
+            startPoint, endPoint = extractEndPoints(row)
+    return trimmedMatrix
 
+def extractEndPoints(firstRow):
+    """
+    Extracts the dimensions of the matrix by utilizing the first row. This finds the first black spot and the last.
+
+    :param firstRow: The first row with data.
+    :return: A tuple of index of the first useful bit and the last useful bit.
+    """
+    wastedSpace = 0
+    for num in firstRow:
+        if num == 255:
+            wastedSpace+=1
+        else:
+            break
+    lastBlackIndex, count = wastedSpace, wastedSpace
+    for num in firstRow[wastedSpace:]:
+        if num == 0:
+            lastBlackIndex = count
+        count+=1
+    return (wastedSpace, lastBlackIndex)
 
 
 def rowIsWhiteSpace(row):
+    """
+    Returns a boolean of whether row is white space.
+
+    :param row: A row of a matrix.
+    :return: True if all white pixels, false otherwise.
+    """
     for i in row:
         if i != 255:
             return False
     return True
 
 if __name__ == "__main__":
-    numpy.set_printoptions(threshold=numpy.nan)
     matrix = (numpy.asarray(Image.open("test.png").convert('L')).tolist())
-
+    matrix = trimWhiteSpace(matrix)
     for i in (matrix):
         print(i)
     #print(trimWhiteRowSpace(matrix))
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # QR-Decoder
+sudo pip install http://effbot.org/media/downloads/Imaging-1.1.7.tar.gz
